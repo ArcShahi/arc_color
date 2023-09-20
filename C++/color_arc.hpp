959652dcb7080710972df26b925cb7930f5df257
ArcShahi <90377780+ArcShahi@users.noreply.github.com>
2023-09-20 16:16:15 +0530

    Update color_arc.hpp

--- C++/color_arc.hpp
+++ C++/color_arc.hpp
@@ -9,22 +9,23 @@ enum class Color : std::uint8_t
 {
 
 	red = 160,
-	rasp=198,
+	rasp = 198,
 	pink = 210,
-    yellow = 11,
-	Lyellow=227,
-	orange=208,
-	blue=27,
-	sky_blue=39,
-	cyan=51,
-	teal=30,
-	grey=8,
-	black=0,
-	green=10,
-	Lgreen=118,
-	violet=99,
-	magenta=5,
+	yellow = 11,
+	Lyellow = 227,
+	orange = 208,
+	blue = 27,
+	sky_blue = 39,
+	cyan = 51,
+	teal = 30,
+	grey = 8,
+	black = 0,
+	green = 10,
+	Lgreen = 118,
+	violet = 99,
+	magenta = 5,
 	white = 15,
+	brown = 94,
 	
 	
 	
@@ -51,8 +52,9 @@ enum class BG : std::uint8_t
 	violet = 99,
 	magenta = 5,
 	white=15,
-};
+	brown = 94,
 
+};
 
 constexpr char ANSI_COLOR_PREFIX[] = "\033[38;5;";
 
@@ -71,12 +73,12 @@ std::ostream& operator<<(std::ostream& out, Color color_code)
 
 }
 
-// for Background color
-std::ostream& operator<<(std::ostream& out, BG BG_color) 
+
+//for background text color
+
+std::ostream& operator<<(std::ostream& out, BG BG_color)
 {
 	return out << ANSI_BG_COLOR_PREFIX << static_cast<short>(BG_color) << 'm';
 }
 
 #endif // ANSI_8BIT_COLORS
-
-
