657c9273fa0c2dd9bf1a67144e64519d9fb204d8
ArcShahi <90377780+ArcShahi@users.noreply.github.com>
2023-12-29 12:04:09 +0530

    Update color_arc.hpp

--- C++/color_arc.hpp
+++ C++/color_arc.hpp
@@ -30,9 +30,9 @@ enum class Color : std::uint8_t
 	magenta = 5,
 	white = 15,
 	brown = 94,
-	
-	
-	
+
+
+
 
 };
 
@@ -40,11 +40,11 @@ enum class BG : std::uint8_t
 {
 
 	red = 160,
-	crimson=124, // new
+	crimson = 124, // new
 	rasp = 198,
 	pink = 201,
-	hot_pink=198, //new
-    yellow = 11, 
+	hot_pink = 198, //new
+	yellow = 11,
 	Lyellow = 227,
 	orange = 208,
 	blue = 27,
@@ -56,36 +56,35 @@ enum class BG : std::uint8_t
 	green = 10,
 	Lgreen = 118,
 	violet = 99,
-	lavender=135, //new
+	lavender = 135, //new
 	magenta = 5,
-	white=15,
+	white = 15,
 	brown = 94,
 
 };
 
-constexpr char ANSI_COLOR_PREFIX[] = "\033[38;5;";
 
-constexpr char ANSI_BG_COLOR_PREFIX[] = "\033[48;5;";
-
-constexpr char RESET[] = "\033[0m"; // for resetting the Terminal Color Formatting
-
-constexpr char clrScr[] = "\033[2J\033[H";
+constexpr char ANSI_CLR_PREFIX[] = "\033[38;5;";
+constexpr char ANSI_BG_CLR_PREFIX[] = "\033[48;5;";
+constexpr char CLS[] = "\033[2J\033[H";
+constexpr char NoClr[] = "\033[0m";
 
 
-// For text Color 
 std::ostream& operator<<(std::ostream& out, Color color_code)
 {
 
-	return out << ANSI_COLOR_PREFIX << static_cast<short>(color_code) << 'm';
+	return out << ANSI_CLR_PREFIX << static_cast<short>(color_code) << 'm';
 
 }
 
-
-//for background text color
-
-std::ostream& operator<<(std::ostream& out, BG BG_color)
+std::ostream& operator<<(std::ostream& out, BG color_code)
 {
-	return out << ANSI_BG_COLOR_PREFIX << static_cast<short>(BG_color) << 'm';
+
+	return out << ANSI_CLR_PREFIX << static_cast<short>(color_code) << 'm';
+
 }
+
+
+
 
 #endif // ANSI_8BIT_COLORS
